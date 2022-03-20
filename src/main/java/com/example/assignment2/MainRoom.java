b7341ac84b6149e51254d3128c534b035fcf9021
bed-com-14-19 <bed-com-14-19@Unima.ac.mw>
2022-03-20 17:32:12 +0200

    I FINALIZED MY PROJECT AT THIS TIME

--- src/main/java/com/example/assignment2/MainRoom.java
+++ src/main/java/com/example/assignment2/MainRoom.java
@@ -10,6 +10,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import javafx.scene.control.Label;
@@ -24,68 +25,75 @@ public class MainRoom extends Application {
     public void start(Stage primaryStage) {
 
         HBox hBox = new HBox();
-        hBox.setStyle("-fx-padding: 2, 3, 6, 15 ");
+        hBox.setStyle("-fx-padding: 3, 5, 9, 13 ");
         hBox.setSpacing(6);
-        Button file = new Button("file");
-        Button edit = new Button("Edit");
-        Button formart = new Button("Format");
-        Button view = new Button("View");
-        Button help = new Button("Help");
-        hBox.getChildren().addAll(file, edit, formart, view, help);
+        Button file = new Button("FILE");
+        Button format = new Button("FORMAT");
+        Button view = new Button("VIEW");
+        Button edit1 = new Button("EDITS");
+        Button help = new Button("HELP?");
+        hBox.getChildren().addAll(file, edit1, format, view, help);
 
 
-        Rectangle room1 = new Rectangle(50, 50, 150, 200);
+        Rectangle room1 = new Rectangle(50, 55, 150, 201);
         room1.setFill(Color.GREY);
-        room1.setStroke(Color.BLACK);
-        Label label1 = new Label("Room 1");
+        room1.setStroke(Color.BLUE);
+        Label label1 = new Label("Room A");
+        label1.setFont(Font.font(10));
         label1.setLayoutX(80);
         label1.setLayoutY(100);
 
 
-        Rectangle room2 = new Rectangle(200, 170, 200, 40);
+        Rectangle room2 = new Rectangle(200, 171, 200, 40);
         room2.setFill(Color.GREY);
-        room2.setStroke(Color.BLACK);
-        Label label2 = new Label("Room 2");
+        room2.setStroke(Color.BLUE);
+        Label label2 = new Label("Room B");
+        label2.setFont(Font.font(10));
         label2.setLayoutX(230);
         label2.setLayoutY(180);
 
 
         Rectangle room3 = new Rectangle(200, 50, 100, 120);
         room3.setFill(Color.GREY);
-        room3.setStroke(Color.BLACK);
-        Label label3 = new Label("Room 3");
+        room3.setStroke(Color.BLUE);
+        Label label3 = new Label("Room C");
+        label3.setFont(Font.font(10));
         label3.setLayoutX(230);
         label3.setLayoutY(130);
 
 
         Rectangle room4 = new Rectangle(300, 50, 100, 120);
         room4.setFill(Color.GREY);
-        room4.setStroke(Color.BLACK);
-        Label label4 = new Label("Room 4");
+        room4.setStroke(Color.BLUE);
+        Label label4 = new Label("Room D");
+        label4.setFont(Font.font(10));
         label4.setLayoutX(320);
         label4.setLayoutY(120);
 
         Rectangle room5 = new Rectangle(400, 50, 120, 160);
         room5.setFill(Color.GREY);
-        room5.setStroke(Color.BLACK);
-        Label label5 = new Label("Room 5");
+        room5.setStroke(Color.BLUE);
+        Label label5 = new Label("Room E");
+        label5.setFont(Font.font(10));
         label5.setLayoutX(420);
         label5.setLayoutY(130);
 
         Rectangle room6 = new Rectangle(200, 210, 160, 150);
         room6.setFill(Color.GREY);
-        room6.setStroke(Color.BLACK);
-        Label label6 = new Label("Room 6");
+        room6.setStroke(Color.BLUE);
+        Label label6 = new Label("Room F");
+        label6.setFont(Font.font(10));
         label6.setLayoutX(300);
         label6.setLayoutY(250);
 
         Rectangle room7 = new Rectangle(360, 210, 160, 150);
         room7.setFill(Color.GREY);
-        room7.setStroke(Color.BLACK);
-        Label label7 = new Label("Room 7");
+        room7.setStroke(Color.BLUE);
+        Label label7 = new Label("Room G");
         label7.setLayoutX(470);
         label7.setLayoutY(250);
-        Button room1btn = new Button("Room1 ON/OFF");
+        label7.setFont(Font.font(10));
+        Button room1btn = new Button("RoomA ON/OFF");
 
         room1btn.setOnAction(e -> {
             if(room1.getFill() == Color.GREY) {
@@ -128,7 +136,7 @@ public class MainRoom extends Application {
                 room6.setFill(Color.GREY);
                 room7.setFill(Color.GREY);
             }
-        }); Button button2 = new Button("Room1&2 ON/OFF");
+        }); Button button2 = new Button("RoomA&B ON/OFF");
         button2.setOnAction(e -> {
             if(room2.getFill() == Color.GREY || room1.getFill() == Color.GREY) {
                 room2.setFill(Color.GREEN);
@@ -140,7 +148,7 @@ public class MainRoom extends Application {
             }
 
         });
-        Button button3 = new Button("Room1 ON/OFF");
+        Button button3 = new Button("RoomA ON/OFF");
         button3.setOnAction(e -> {
             if(room1.getFill() == Color.GREY) {
                 room1.setFill(Color.YELLOW);
@@ -149,7 +157,7 @@ public class MainRoom extends Application {
                 room1.setFill(Color.GREY);
             }
         });
-        Button button4 = new Button("Room2 ON/OFF");
+        Button button4 = new Button("RoomB ON/OFF");
         button4.setOnAction(e -> {
             if(room2.getFill() == Color.GREY) {
                 room2.setFill(Color.GREEN);
@@ -158,7 +166,7 @@ public class MainRoom extends Application {
                 room2.setFill(Color.GREY);
             }
         });
-        Button button5 = new Button("Room3 ON/OFF");
+        Button button5 = new Button("RoomC ON/OFF");
         button5.setOnAction(e -> {
             if(room3.getFill() == Color.GREY) {
                 room3.setFill(Color.ORANGE);
@@ -167,7 +175,7 @@ public class MainRoom extends Application {
                 room3.setFill(Color.GREY);
             }
         });
-        Button button6 = new Button("Room4 ON/OFF");
+        Button button6 = new Button("RoomD ON/OFF");
         button6.setOnAction(e -> {
             if(room4.getFill() == Color.GREY) {
                 room4.setFill(Color.ORANGE);
@@ -176,7 +184,7 @@ public class MainRoom extends Application {
                 room4.setFill(Color.GREY);
             }
         });
-        Button button7 = new Button("Room5 ON/OFF");
+        Button button7 = new Button("RoomE ON/OFF");
         button7.setOnAction(e -> {
             if(room5.getFill() == Color.GREY) {
                 room5.setFill(Color.YELLOW);
@@ -185,7 +193,7 @@ public class MainRoom extends Application {
                 room5.setFill(Color.GREY);
             }
         });
-        Button button8 = new Button("Room6 ON/OFF");
+        Button button8 = new Button("RoomF ON/OFF");
         button8.setOnAction(e -> {
             if(room6.getFill() == Color.GREY) {
                 room6.setFill(Color.RED);
@@ -194,7 +202,7 @@ public class MainRoom extends Application {
                 room6.setFill(Color.GREY);
             }
         });
-        Button button9 = new Button("Room7 ON/OFF");
+        Button button9 = new Button("RoomG ON/OFF");
         button9.setOnAction(e -> {
             if(room7.getFill() == Color.GREY) {
                 room7.setFill(Color.YELLOW);
@@ -222,8 +230,8 @@ public class MainRoom extends Application {
         borderPane.setTop(hBox);
         borderPane.setCenter(pane);
         borderPane.setBottom(gridPane);
-        Scene scene = new Scene( borderPane,500, 600);
-        primaryStage.setTitle("MyHouse");
+        Scene scene = new Scene( borderPane,510, 300);
+        primaryStage.setTitle("MAIN ROOM");
         primaryStage.setScene(scene);
         primaryStage.show();
 
